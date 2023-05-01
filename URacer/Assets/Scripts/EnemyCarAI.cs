35eacbaa98b259f774bd592a130e9978e69ec4f4
Galanafai Windross <galanafai@gmail.com>
2023-05-01 02:39:27 -0700

    "Enemy car prefab with explosion"

--- URacer/Assets/Scripts/EnemyCarAI.cs
+++ URacer/Assets/Scripts/EnemyCarAI.cs
@@ -4,40 +4,105 @@ using UnityEngine;
 
 public class EnemyCarAI : MonoBehaviour
 {
-    public Transform player;
-    public float chaseRadius = 10.0f;
-    public float attackSpeed = 50.0f;
+    [SerializeField] private string playerTag = "Player"; // Tag of the player game object
+    [SerializeField] private float chaseRadius = 10.0f;
+    [SerializeField] private float attackForce = 15;
+    [SerializeField] private float explosionForce = 50000.0f;
+    [SerializeField] private GameObject explosionPrefab;
+
+    private bool isMovingTowardsTarget = false; // flag to indicate whether the enemy car is currently moving towards the target
+    private Vector3 targetPosition; // position towards which the enemy car will move when it is within the target's radius
 
     private Rigidbody rb;
+    private Transform playerTransform;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        GameObject playerObject = GameObject.FindGameObjectWithTag(playerTag);
+        if (playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogWarning("No game object with tag '" + playerTag + "' found!");
+        }
     }
 
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
-        if (distanceToPlayer <= chaseRadius)
-        {
-            AttackPlayer();
-        }
+        AttackPlayer();
     }
+    void Death()
+    {
+        // Instantiate the explosion prefab at the position of the enemy car
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        //playerTransform.GetComponent<Rigidbody>().AddForce(directionToPlayer * attackForce * explosionForce, ForceMode.Impulse); // apply a force to push back the player
+        float pushDuration = 100000.0f; // duration of the push in seconds
+        float pushForce = attackForce * explosionForce; // magnitude of the push force
+        StartCoroutine(PushPlayer(directionToPlayer, pushDuration, pushForce));
+    }
+    private IEnumerator PushPlayer(Vector3 direction, float duration, float force)
+    {
+        Rigidbody playerRb = playerTransform.GetComponent<Rigidbody>();
+        playerRb.AddExplosionForce(force, playerTransform.position, 5.0f);
+        yield return new WaitForSeconds(duration);
+        playerRb.velocity = Vector3.zero;
+    }
+
 
     void AttackPlayer()
     {
+        if (playerTransform == null)
+        {
+            return;
+        }
 
-        // Calculate the direction from the enemy car to the player car
-        Vector3 direction = (player.position - transform.position).normalized;
+        float distanceToTarget = Vector3.Distance(transform.position, playerTransform.position);
 
-        // Move the enemy car towards the player car using Lerp
-        Vector3 targetVelocity = direction * attackSpeed;
-        rb.velocity = Vector3.Lerp(rb.velocity, targetVelocity, Time.deltaTime * attackSpeed);
+        if (distanceToTarget <= chaseRadius)
+        {
+            if (!isMovingTowardsTarget)
+            {
+                isMovingTowardsTarget = true;
+                targetPosition = playerTransform.position;
+            }
+        }
+        else
+        {
+            isMovingTowardsTarget = false;
+        }
 
-        // Rotate the enemy car to face the player car
-        Quaternion targetRotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * attackSpeed);
+        if (isMovingTowardsTarget)
+        {
+            Vector3 playerVelocity = playerTransform.GetComponent<Rigidbody>().velocity;
+            Vector3 predictedPlayerPosition = playerTransform.position + playerVelocity * (distanceToTarget / attackForce);
+
+            Vector3 directionToTarget = (predictedPlayerPosition - transform.position).normalized;
+            Vector3 directionToTargetLeft = Quaternion.AngleAxis(-90.0f, Vector3.up) * directionToTarget;
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTargetLeft, Vector3.up);
+            transform.rotation = targetRotation;
+
+            Vector3 rotatedForward = Quaternion.Euler(0, 90, 0) * transform.forward;
+
+            
+            transform.position += rotatedForward * attackForce * Time.deltaTime;
+            if (Vector3.Distance(transform.position, playerTransform.position) <= 1.0f) // if the enemy car reaches the target
+            {
+                Death();
+                /*Destroy(gameObject);
+                // destroy the enemy car
+                
+                */
+                
+            }
+
+        }
     }
+
+
 
 }
