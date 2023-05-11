b981bad4ed414a93c4d564c118432b64fee099d7
aaron <aedlebeck@gmail.com>
2023-05-11 01:56:04 -0700

    Checkpoint reset

--- URacer/Assets/CheckpointController.cs
+++ URacer/Assets/CheckpointController.cs
@@ -8,12 +8,11 @@ public class CheckpointController : MonoBehaviour
     public GameObject start;
     public GameObject end;
     public GameObject[] checkpoints;
-    private Vector3 curCheckpoint;
-    // public GameObject car;
+    private GameObject curCheckpoint;
     Rigidbody rb;
 
     [Header("Settings")]
-    public float laps = 1;
+    public float laps = 100;
 
     [Header("Information")]
     private int currentCheckpoint;
@@ -36,7 +35,7 @@ public class CheckpointController : MonoBehaviour
         currentLapTime = 0;
         bestLapTime = 0;
         bestLap = 0;
-        curCheckpoint = start.transform.position;
+        curCheckpoint = start;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -49,18 +48,14 @@ public class CheckpointController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            // Rigidbody rb = GetComponent<Rigidbody>();
             rb.isKinematic = true;
             print("return key was pressed");
-            Vector3 pos = curCheckpoint + new Vector3(0,5,0);
+            Vector3 pos = curCheckpoint.transform.position + new Vector3(0,0,0);
             Vector3 position =  Vector3.MoveTowards(pos, rb.position, 1f * Time.fixedDeltaTime);
-            // rb.position = pos;
-            // rb.AddForce(0f, 0f, 0f);
             rb.MovePosition(position);
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
-            rb.rotation = Quaternion.Euler(new Vector3(0,0,0));
-            // rb.isKinematic = false;
+            rb.rotation = curCheckpoint.transform.rotation;
             Invoke("setKine", 0.1f);
         }
 
@@ -92,33 +87,36 @@ public class CheckpointController : MonoBehaviour
             // Started the race
             if (thisCheckpoint == start && !started)
             {
-                curCheckpoint = thisCheckpoint.transform.position;
+                curCheckpoint = thisCheckpoint;
                 print("Started");
                 started = true;
+                currentLapTime = 0;
             }
             // Ended the lap or race
-            else if (thisCheckpoint == end && started)
-            {
+            // else if (thisCheckpoint == end && started)
+            // {
                 // If all the laps are finished, end the race
-                if (currentLap == laps)
-                {
-                    if (currentCheckpoint == checkpoints.Length)
-                    {
-                        if (currentLapTime < bestLapTime)
-                        {
-                            bestLap = currentLap;
-                        }
+                // if (currentLap == laps)
+                // {
+                //     if (currentCheckpoint == checkpoints.Length)
+                //     {
+                //         if (currentLapTime < bestLapTime)
+                //         {
+                //             bestLap = currentLap;
+                //         }
 
-                        finished = true;
-                        print("Finished");
-                    }
-                    else
-                    {
-                        print("Did not go through all checkpoints");
-                    }
-                }
+                //         finished = true;
+                //         currentCheckpoint = 0;
+                //         print("Finished");
+                //     }
+                //     else
+                //     {
+                //         print("Did not go through all checkpoints");
+                //     }
+                // }
                 // If all laps are not finished, start a new lap
-                else if (currentLap < laps)
+                /*else*/ 
+                if (currentLap < laps)
                 {
                     if (currentCheckpoint == checkpoints.Length)
                     {
@@ -135,10 +133,10 @@ public class CheckpointController : MonoBehaviour
                     }
                     else
                     {
-                        print("Did not go through all checkpoints");
+                        print($"Did not go through all checkpoints");
                     }
                 }
-            }
+            // }
 
             // Loop through the checkpoints to compare and check which one the player touched
             for (int i = 0; i < checkpoints.Length; i++)
@@ -149,8 +147,7 @@ public class CheckpointController : MonoBehaviour
                 // If the checkpoint is correct
                 if (thisCheckpoint == checkpoints[i] && i + 1 == currentCheckpoint + 1)
                 {
-                    curCheckpoint = thisCheckpoint.transform.position;
-                    // thisCheckpoint.SetActive(false);
+                    curCheckpoint = thisCheckpoint;
                     print($"Correct Checkpoint: {Mathf.FloorToInt(currentLapTime / 60)}:{currentLapTime % 60:00.000}");
                     currentCheckpoint++;
                 }
@@ -160,17 +157,19 @@ public class CheckpointController : MonoBehaviour
                     print($"Incorrect checkpoint");
                 }
             }
+            print($"{currentCheckpoint} / {checkpoints.Length}");
+
         }
     }
 
-    // private void OnGUI()
-    // {
+    private void OnGUI()
+    {
     //     // Current time
-    //     string formattedCurrentLapTime = $"Current: {Mathf.FloorToInt(currentLapTime / 60)}:{currentLapTime % 60:00.000} - (Lap {currentLap})";
-    //     GUI.Label(new Rect(50, 10, 250, 100), formattedCurrentLapTime);
+        string formattedCurrentLapTime = $"Current: {Mathf.FloorToInt(currentLapTime / 60)}:{currentLapTime % 60:00.000} - (Lap {currentLap})";
+        GUI.Label(new Rect(50, 50, 250, 100), formattedCurrentLapTime);
 
     //     // Best time
-    //     string formattedBestLapTime = $"Best: {Mathf.FloorToInt(bestLapTime / 60)}:{bestLapTime % 60:00.000} - (Lap {bestLap})";
-    //     GUI.Label(new Rect(250, 10, 250, 100), (started) ? formattedBestLapTime : "0:00.000");
-    // }
+        string formattedBestLapTime = $"Best: {Mathf.FloorToInt(bestLapTime / 60)}:{bestLapTime % 60:00.000} - (Lap {bestLap})";
+        GUI.Label(new Rect(100, 100, 250, 100), (started) ? formattedBestLapTime : "0:00.000");
+    }
 }
